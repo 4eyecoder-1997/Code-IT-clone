b9daa5447a8c0146effb809b354deb20e3486957
4 eye coder <4eyecoder@gmail.com>
2022-02-02 21:13:26 +0545

    day-2 extra js

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
-const dropBtn = document.querySelector('.drop-btn');
+// active class navigation menu
+let menuButtons = document.querySelectorAll('.navigation-link');
 
-dropBtn.addEventListener('click', ()=> {
-    const dropDown = document.querySelector('.drop-down');
-
-    if (dropDown.style.display === 'none') {
-        dropDown.style.display === 'display';
-    } else {
-        dropDown.style.display === 'none';
-    }
+// loop through all navigation link 
+menuButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        menuButtons.forEach((btn)=> {
+            btn.classList.remove('active');
+            this.classList.add('active');
+        });
+    });
 });
+
+// image sliders / carousel
+let slideIndex = 0;
+
+function controller(x) {
+    slideIndex = slideIndex + x;
+    slideShow(slideIndex);
+}   
+
+slideShow(slideIndex);
+
+function slideShow(num) {
+    const slides = document.getElementsByClassName('slider-box');
+
+    if(num == slides.length) {
+        slideIndex = 0;
+        num = 0;
+    } else if (num < 0) {
+        slideIndex = slides.length - 1;
+        num = slides.length - 1;
+    }
+
+    for(let i of slides) {
+        i.style.display = 'none';
+    }
+
+    slides[num].style.display = 'block';
+}
+
+
