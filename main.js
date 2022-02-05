f8ec8ab9f050bb3484866c36dbf823d506a549b1
4 eye coder <4eyecoder@gmail.com>
2022-02-05 22:16:50 +0545

    day-4 section 7

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 let menuButtons = document.querySelectorAll('.navigation-link');
 
 // loop through all navigation link 
-menuButtons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        menuButtons.forEach((btn)=> {
+menuButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        menuButtons.forEach((btn) => {
             btn.classList.remove('active');
             this.classList.add('active');
         });
@@ -17,14 +17,14 @@ let slideIndex = 0;
 function controller(x) {
     slideIndex = slideIndex + x;
     slideShow(slideIndex);
-}   
+}
 
 slideShow(slideIndex);
 
 function slideShow(num) {
     const slides = document.getElementsByClassName('slider-box');
 
-    if(num == slides.length) {
+    if (num == slides.length) {
         slideIndex = 0;
         num = 0;
     } else if (num < 0) {
@@ -32,11 +32,9 @@ function slideShow(num) {
         num = slides.length - 1;
     }
 
-    for(let i of slides) {
+    for (let i of slides) {
         i.style.display = 'none';
     }
 
     slides[num].style.display = 'block';
 }
-
-
